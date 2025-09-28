8b78aee3b1c5648468bcfa81cacafec52044f294
kliull <semenikhinaAA09@yandex.ru>
2025-09-29 00:28:05 +0300

    task3

---
+++ Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace third
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int secretDigit = rnd.Next(1, 11);
+            Console.WriteLine("Попробуйте отгадать число от 1 до 10 которое я загадал.");
+            string digitInput = Console.ReadLine();
+            int digit = int.Parse(digitInput);
+            while (secretDigit != digit)
+            {
+                if (secretDigit > digit)
+                {
+                    Console.WriteLine(">");
+                }
+                else
+                {
+                    Console.WriteLine("<");
+                }
+                digitInput = Console.ReadLine();
+                digit = int.Parse(digitInput);
+            }
+            Console.WriteLine("Вы угадали!");
+        }
+    }
+}
