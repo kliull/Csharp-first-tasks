41c7ec3010eaafcbabca53cc45ee4c85c293d06a
kliull <semenikhinaAA09@yandex.ru>
2025-09-29 00:15:48 +0300

    task2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace second
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.WriteLine("Введите число. Если захотите остановить программу введите 0");
+            string digitInput = Console.ReadLine();
+            while (digitInput != "0")
+            {
+                int digit = int.Parse(digitInput);
+                sum += digit;
+                Console.WriteLine($"Текущая сумма: {sum}");
+                Console.WriteLine("Введите число. Если захотите остановить программу введите 0");
+                digitInput = Console.ReadLine();
+            }
+        }
+    }
+}
