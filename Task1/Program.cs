60958dff5a902e10d74a320ac4e38a4b2eb79e59
kliull <semenikhinaAA09@yandex.ru>
2025-09-29 00:15:33 +0300

    task1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace first
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите градусы в системе Фаренгейта");
+            string degreesCInput = Console.ReadLine();
+            double degreesC = Convert.ToInt32(degreesCInput);
+            double degreesF = (degreesC * 1.8) + 32;
+            Console.WriteLine($"{degreesC}°C = {degreesF}°F");
+        }
+    }
+}
