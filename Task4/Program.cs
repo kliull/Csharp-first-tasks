8268e8892d0d26621aa0d8173052172178e79886
kliull <semenikhinaAA09@yandex.ru>
2025-09-29 11:58:35 +0300

    task4

---
+++ Task4/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace third
+{
+    class Program
+    {
+        static int Count(int countSymb, string listSymb, string password)
+        {
+            foreach (char element in password)
+            {
+                foreach (char symbol in listSymb)
+                {
+                    if (element == symbol)
+                    {
+                        countSymb += 1;
+                    }
+                }
+            }
+            return countSymb;
+        }
+        static void Main(string[] args)
+        {
+            bool flag = false;
+            Console.WriteLine("Напишите пароль");
+            string password = Console.ReadLine();
+            string symbols = "!@#$%^&*";
+            string letters = "QWERTYUIOPASDFGHJKLZXCVBNM";
+            string digits = "0123456789";
+
+            while (flag != true)
+            {
+                int symbolsCount = 0;
+                int lettersCount = 0;
+                int digitsCount = 0;
+                symbolsCount = Count(symbolsCount, symbols, password);
+                lettersCount = Count(lettersCount, letters, password);
+                digitsCount = Count(digitsCount, digits, password);
+                if (password.Length < 8)
+                {
+                    Console.WriteLine("Слишком короткий. Необходимо больше 8 символов");
+                }
+                else if (digitsCount < 1)
+                {
+                    Console.WriteLine("не содержит ни одной цифры. Необходима 1 цифра");
+                }
+                else if (lettersCount < 1)
+                {
+                    Console.WriteLine("не содержит ни одной заглавной буквы. Необходима 1 заглавная буква.");
+                }
+                else if (symbolsCount < 1)
+                {
+                    Console.WriteLine("не содержит ни одного спец знака. Необходим 1 символ из (!@#$%^&*)");
+                }
+                else
+                {
+                    flag = true;
+                }
+                if (flag == true)
+                {
+                    Console.WriteLine("Надежный пароль!");
+                    break;
+                }
+                password = Console.ReadLine();
+            }
+
+        }
+    }
+}
