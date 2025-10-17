a8ba70545d7110dc04c9c51eff89a600a6acdf88
kliull <semenikhinaAA09@yandex.ru>
2025-10-17 21:16:57 +0300

    task4 fix

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 
 namespace third
 {
@@ -20,42 +20,43 @@ namespace third
         }
         static void Main(string[] args)
         {
-            bool flag = false;
+            int flagcount = 0;
             Console.WriteLine("Напишите пароль");
             string password = Console.ReadLine();
             string symbols = "!@#$%^&*";
             string letters = "QWERTYUIOPASDFGHJKLZXCVBNM";
             string digits = "0123456789";
 
-            while (flag != true)
+            while (flagcount != 4)
             {
                 int symbolsCount = 0;
                 int lettersCount = 0;
                 int digitsCount = 0;
+                flagcount = 4;
                 symbolsCount = Count(symbolsCount, symbols, password);
                 lettersCount = Count(lettersCount, letters, password);
                 digitsCount = Count(digitsCount, digits, password);
                 if (password.Length < 8)
                 {
                     Console.WriteLine("Слишком короткий. Необходимо больше 8 символов");
+                    flagcount -= 1;
                 }
                 if (digitsCount < 1)
                 {
                     Console.WriteLine("Не содержит ни одной цифры. Необходима 1 цифра");
+                    flagcount -= 1;
                 }
                 if (lettersCount < 1)
                 {
                     Console.WriteLine("Не содержит ни одной заглавной буквы. Необходима 1 заглавная буква.");
+                    flagcount -= 1;
                 }
                 if (symbolsCount < 1)
                 {
                     Console.WriteLine("Не содержит ни одного спец знака. Необходим 1 символ из (!@#$%^&*)");
+                    flagcount -= 1;
                 }
-                else
-                {
-                    flag = true;
-                }
-                if (flag == true)
+                if (flagcount == 4)
                 {
                     Console.WriteLine("Надежный пароль!");
                     break;
